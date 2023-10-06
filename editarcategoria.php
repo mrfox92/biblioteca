ca8710e1b64db0f0064b7157b202ec8e34a4c404
David Alvarez <davidalvarez54@gmail.com>
2023-10-06 11:26:54 -0300

    Agregada funcionalidad editar categoria

--- editarcategoria.php
+++ editarcategoria.php
@@ -1,9 +1,41 @@
 <?php
 //  incluir conexion a BD
 include 'conexion.php';
+//  llamamos a nuestro archivo partials para mostrar las notificaciones
+include './partials/scripts.php';
+
+//  Comprobamos si vienen datos desde formulario a través del método POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id = $_POST['id'];
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
+
+    // Actualizar los datos
+    try {
+
+        $sql = 'UPDATE categoria SET nombre = :nombre, descripcion = :descripcion WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+        $stmt->bindParam(':descripcion', $descripcion, PDO::PARAM_STR);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        //  muestra notificacion y redirecciona a categoria.php
+        echo "<script>
+                Swal.fire('Categoria editada', 'La categoría ha sido editada correctamente', 'success').then(function() {
+                    location.href = 'editarcategoria.php?id={$id}'
+                });
+            </script>";
+
+    } catch (PDOException $e) {
+        echo "Error al actualizar categoria: " . $e->getMessage();
+    }
+}
 
 // Obtén datos de la categoría a editar
-if (isset($_GET['id']))
+if (isset($_GET['id']) && !empty($_GET['id']))
 {
     $idCategoria = $_GET['id'];
 
@@ -16,7 +48,7 @@ if (isset($_GET['id']))
 
         $categoria = $stmt->fetch(PDO::FETCH_ASSOC);
         $message = isset($categoria) && !empty($categoria) ? "Categoria obtenida" : "Categoria no existe";
-        echo $message;
+        // echo '<h1 style="text-align: center;">' . $message  . " " . $categoria['nombre'] .'</h1>';
 
     } catch ( PDOException $e ) {
         echo "Error al obtener los datos de la categoría: " . $e->getMessage();
@@ -52,17 +84,17 @@ if (isset($_GET['id']))
 
 
                 <h1>Editar Categoría</h1>
-                <form method="POST" action="">
-                    <input type="hidden" name="id" value="<?php echo $categoria['id']; ?>">
+                <form method="POST" action="editarcategoria.php">
+                    <input type="hidden" name="id" value="<?php echo (isset($categoria['id'])) ? $categoria['id'] : ""; ?>">
                     
                     <div class="mb-3">
                         <label for="nombre">Nombre:</label>
-                        <input class="form-control" type="text" id="nombre" name="nombre" value="<?php echo $categoria['nombre']; ?>" required>
+                        <input class="form-control" type="text" id="nombre" name="nombre" value="<?php echo (isset($categoria['nombre'])) ? $categoria['nombre'] : ""; ?>" required>
                     </div>
             
                     <div class="mb-3">    
                         <label for="descripcion" class="form-label">Descripción:</label>
-                        <textarea class="form-control" id="descripcion" name="descripcion" required><?php echo $categoria['descripcion']; ?></textarea>
+                        <textarea class="form-control" id="descripcion" name="descripcion" required><?php echo (isset($categoria['descripcion'])) ? $categoria['descripcion'] : ""; ?></textarea>
                     </div>
             
                     <div class="d-grid gap-2">
